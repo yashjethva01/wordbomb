0c6b51f5840bfd2a730635c64c702b106fbbd7dc
Yash Jethva <jethvayash09@gmail.com>
2026-04-20 02:59:47 +0530

    fix server crash

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,18 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
 
-  // Allow JSX syntax inside plain .js files (hooks, services, utils)
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: { '.js': 'jsx' },
-    },
-  },
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.jsx?$/,
-    exclude: [],
-  },
-
   server: {
     port: 5173,
     proxy: {
